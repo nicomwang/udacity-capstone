97b969a11d12bca0638553917f2672c5a3e32a6e
nicomwang <nicomwang@gmail.com>
2021-12-17 20:20:52 -0500

    Test rolling update

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello, my name is Nico!');
+  res.send('Hello, my name is Nico! This is V2 from rolling update!');
 });
 
 app.listen(PORT, HOST);
